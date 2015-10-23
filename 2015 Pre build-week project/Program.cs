fe23d379a7e0b15713a08aae9bd4ed5cad8e014d
lessthantrue <nickm@outofthisworld.net>
2015-10-22 20:54:11 -0700

    Initial Commit
    Standard, blank C# Project. Let's get started!

---
+++ 2015 Pre build-week project/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using WPILib;
+
+namespace _2015_Pre_build_week_project
+{
+    public class Program
+    {
+        static void Main()
+        {
+            RobotBase.Main(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ 2015 Pre build-week project/_2015_Pre_build_week_project.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WPILib;
+
+namespace _2015_Pre_build_week_project
+{
+    /**
+     * The VM is configured to automatically run this class, and to call the
+     * functions corresponding to each mode, as described in the IterativeRobot
+     * documentation. 
+     */
+    public class _2015_Pre_build_week_project : IterativeRobot
+    {
+        /**
+         * This function is run when the robot is first started up and should be
+         * used for any initialization code.
+         */
+        public override void RobotInit()
+        {
+
+        }
+
+        /**
+         * This function is called periodically during autonomous
+         */
+        public override void AutonomousPeriodic()
+        {
+
+        }
+
+        /**
+         * This function is called periodically during operator control
+         */
+        public override void TeleopPeriodic()
+        {
+
+        }
+
+        /**
+         * This function is called periodically during test mode
+         */
+        public override void TestPeriodic()
+        {
+
+        }
+    }
+}
