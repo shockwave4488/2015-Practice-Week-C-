faa04eb93552cae3923fdfe6e8c8932cc0865e16
Isaac Pavon <isaac_pavon@yahoo.com>
2015-11-12 19:01:03 -0800

    Controller class
    Adds controller class and vibrate function

---
+++ 2015 Pre build-week project/SubSystems/Controllers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WPILib;
+
+namespace _2015_Pre_build_week_project.SubSystems
+{
+    class Controllers
+    {
+        public void Vibrate()
+        {
+            if (ControllerPower.GetInputVoltage() < Constants.VibrateVoltLimit)
+            {
+                xbox.SetRumble(Joystick.RumbleType.LeftRumble, 1);
+                xbox.SetRumble(Joystick.RumbleType.RightRumble, 1);
+            }
+            else
+            {
+                xbox.SetRumble(Joystick.RumbleType.LeftRumble, 0);
+                xbox.SetRumble(Joystick.RumbleType.RightRumble, 0);
+            }
+
+        }
+    }
+}
