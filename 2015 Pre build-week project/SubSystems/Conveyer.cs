38c19509167e112c04660c55cf275d17074ce0e0
Isaac Pavon <isaac_pavon@yahoo.com>
2015-11-20 19:12:27 -0800

    hgfh
    hdfhfd

--- 2015 Pre build-week project/SubSystems/Conveyer.cs
+++ 2015 Pre build-week project/SubSystems/Conveyer.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WPILib;
 
 namespace _2015_Pre_build_week_project.SubSystems
 {
     class Conveyer
     {
+        private Talon conveyor;
+        private DigitalInput beamBrakOut;
+        private DigitalInput beamBrakeIn;
+        public Conveyer()
+        {
+            conveyor = new Talon(Constants.Conveyer_Channel);
+            beamBrakeIn = new DigitalInput(Constants.Conveyer_BeambrakeInChannel);
+            beamBrakeOut = new DigitalInput(Constants.Conveyer_BeambrakeInChannel);
+
+        }
+
     }
 }
